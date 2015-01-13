c20d244c562cd3aa9b7fc7611e11307ea687d352
Skylight <chevalier.benjamin.fr@gmail.com>
2015-01-13 12:19:31 +0100

    ajout page index avec essai formulaire

--- src/main/java/fr/iutinfo/utils/TestResource.java
+++ src/main/java/fr/iutinfo/utils/TestResource.java
@@ -12,6 +12,26 @@ public class TestResource {
 	@GET
 	@Produces(MediaType.TEXT_HTML)
 	public String getHello() {
-		return "<html> Hello World ! </html>";
+		return "<html> " +
+				"<head><h1> Studies War</h1></head> " +
+				"<body>" +
+				"<form action=\"/ma-page-de-traitement\" method=\"post\">" +
+				"<div>" +
+				"<label for=\"nom\">Nom :</label>" +
+				"<input type=\"text\" id=\"nom\" />" +
+				"</div>" +
+				"<div>" +
+				"<label for=\"courriel\">Courriel :</label>" +
+				"<input type=\"email\" id=\"courriel\" />" +
+				"</div>" +
+				"<div>" +
+				"<label for=\"message\">Message :</label>" +
+				"<textarea id=\"message\"></textarea>" +
+				"</div>" +
+				"<div class=\"button\">" +
+				"<button type=\"submit\">Envoyer votre message</button>" +
+				"</div>" +
+				"</form>" +
+				"</body></html>";
 	}
 }
