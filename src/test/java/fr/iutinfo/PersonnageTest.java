b64e402f994f0614a636a0ccf457b34c110c2894
Cuvilliers Christophe <cuvilliers.christophe@gmail.com>
2015-01-13 09:32:18 +0100

    Ajout de la classe Personnage2

--- src/test/java/fr/iutinfo/PersonnageTest.java
+++ src/test/java/fr/iutinfo/PersonnageTest.java
@@ -1,7 +1,14 @@
 package fr.iutinfo;
 
+import fr.iutinfo.utils.Personnage;
 import junit.framework.Test;
 
 public class PersonnageTest {
 	
+
+	@Test
+	public void testStats() {
+		Personnage p = new Personnage();
+		
+	}
 }
