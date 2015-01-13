e5e8c95d25a3f7a085c6a6d9827655aaab22bb46
Cuvilliers Christophe <cuvilliers.christophe@gmail.com>
2015-01-13 16:06:33 +0100

    Actions2

--- src/main/java/fr/iutinfo/utils/Action.java
+++ src/main/java/fr/iutinfo/utils/Action.java
@@ -1,12 +1,12 @@
 package fr.iutinfo.utils;
 
 public abstract class Action {
-	
 	Personnage p ;
 	
 	public void decrement(int i){
 		p.setPA(p.getPA()-i);
 	}
 	
+	public abstract void agit();
 
 }
--- src/main/java/fr/iutinfo/utils/Personnage.java
+++ src/main/java/fr/iutinfo/utils/Personnage.java
@@ -20,7 +20,7 @@ public abstract class Personnage {
 		this.stats.put("Maths", r.nextInt(19));
 		this.stats.put("Physique", r.nextInt(19));
 		this.stats.put("SVT", r.nextInt(19));
-		this.stats.put("Algo", r.nextInt(19));
+		this.stats.put("Etudier", r.nextInt(19));
 		this.stats.put("Triche", r.nextInt(19));
 	}
 	
@@ -32,7 +32,7 @@ public abstract class Personnage {
 		this.stats.put("Maths",r.nextInt(19));
 		this.stats.put("Physique", r.nextInt(19));
 		this.stats.put("SVT", r.nextInt(19));
-		this.stats.put("Algo", r.nextInt(19));
+		this.stats.put("Etudier", r.nextInt(19));
 		this.stats.put("Triche", r.nextInt(19));
 	}
 	
@@ -87,6 +87,10 @@ public abstract class Personnage {
 	public ArrayList<Action> getAction() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+	
+	public void addEffect(Effet e){
+		ef.add(e);
 	}
 	
 	
