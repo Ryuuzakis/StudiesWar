978a6fbd0b657a4164e64b7cdf063b461e99cf80
Jonathan Lecointe <jonathan.lecointe@etudiant.univ-lille1.fr>
2015-01-13 08:50:24 +0100

    Initialisation projet

---
+++ src/main/java/fr/iutinfo/App.java
@@ -0,0 +1,17 @@
+package fr.iutinfo;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/v1/")
+public class App extends Application{
+    @Override
+    public Set<Class<?>> getClasses() {
+    	Set<Class<?>> s = new HashSet<Class<?>>();
+    	s.add(UserResource.class);
+    	return s;
+    }
+}
