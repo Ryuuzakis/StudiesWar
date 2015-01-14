64cd955c973cba9493755ce681545b2549db8b5d
Eliott BRICOUT <eliott.bricout@etudiant.univ-lille1.fr>
2015-01-14 13:25:47 +0100

    event

--- src/main/java/fr/iutinfo/studiesWar/event/Evenement.java
+++ src/main/java/fr/iutinfo/studiesWar/event/Evenement.java
@@ -3,12 +3,14 @@ package fr.iutinfo.studiesWar.event;
 import fr.iutinfo.studiesWar.models.Partie;
 
 public abstract class Evenement {
-	private Partie partie;
+	protected Partie partie;
 	private String description;
 	
 	public Evenement(Partie p,String s){
 		this.partie=p;
 	}
+	
+	public abstract boolean alieu();
 	
 	public String toString(){
 		return description;
