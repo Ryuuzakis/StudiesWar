4c81adf7e3d991458aeac629055ef2e5ffdfcc28
Florian Guilbert <floguilbert@gmail.com>
2015-01-14 13:05:56 +0100

    fond d'ecran

--- src/main/webapp/all.js
+++ src/main/webapp/all.js
@@ -48,7 +48,40 @@ function lancerPartie() {
 	alert('Lancer la partie');
 }
 
+/** Fonction basculant la visibilité d'un élément dom
+* @parameter anId string l'identificateur de la cible à montrer, cacher
+*/
+function Hide (addr) { 
+	document.getElementById(addr).style.visibility = "hidden";
+}
 
+function Show (addr) { 
+	document.getElementById(addr).style.visibility = "visible";
+}
+
+function toggle(anId){
+	if (document.getElementById(anId).style.visibility == "hidden")	{
+		Show(anId);
+	}else{	
+		Hide(anId);
+	}
+}
+
+function toggle(anId, anId2){
+	if (document.getElementById(anId).style.visibility == "hidden")	{
+		Show(anId);
+		document.getElementById(anId).style.height = "auto";
+	}else{	
+		Hide(anId);
+		document.getElementById(anId2).style.height = "0";
+	}if (document.getElementById(anId2).style.visibility == "hidden"){
+		Show(anId);
+		document.getElementById(anId).style.height = "auto";
+	}else{	
+		Hide(anId2);
+		document.getElementById(anId2).style.height = "0";
+	}
+}
 
 
 
