29e5d504917634c585d7c479365693dcf4552441
Jonathan Lecointe <jonathan.lecointe@etudiant.univ-lille1.fr>
2015-01-16 11:13:08 +0100

    ajout de methodes pour le tchat

--- src/main/webapp/all.js
+++ src/main/webapp/all.js
@@ -25,6 +25,24 @@ function validerTour() {
 	});
 }
 
+function getMessage(idPartie){
+	var path="v1/partie"+idPartie+"/tchat";
+	$.getJSON(path,function(data){
+		alert("bienvenue");
+		PostMessage(data)
+	});
+}
+
+function PostMessage(idPartie,idJoueur,reponse){
+
+	var html="<p id='chatbox'>";
+	for(var i=0;i<data.caracs.length;++i){
+		html+=data.caracs[i];
+	}
+	html+="</p>";
+	$("#usermsg").html(html);
+}
+
 function getUser(name) {
 	$.getJSON("v1/user/" + name, function(data) {
 		afficheUser(data)
